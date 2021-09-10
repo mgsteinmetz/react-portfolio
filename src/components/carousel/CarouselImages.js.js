5e53a840d99973bf65a6310e165c0042a410aabd
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-09 23:04:49 -0500

    fixing slideshow code

--- src/components/carousel/CarouselImages.js.js
+++ src/components/carousel/CarouselImages.js.js
@@ -9,36 +9,36 @@ import Hello from '../images/Hello.jpg'
 import Pencil from '../images/Pencil.jpg'
 import laptop1 from '../images/laptop1.jpg'
 
- class CarouselImages extends Component {
-     render(){
-    return (
-        <div className={classes.carousel_container} id="start">
-                {/* <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}> */}
-            <Carousel className={classes.carousel} dynamicHeight infiniteLoop={true} interval={6000} useKeyboardArrows={true} transitionTime={1700} emulateTouch showArrows={false} autoPlay  showStatus={false} showThumbs={false} >
-                <div  className={classes.image_container} >
-                    <img className={classes.image} src={Hello}  alt="myImage"/>
-                    <div  className={classes.h1}>
-                       <h1 >I'm Sachin</h1>
-                       <a href="https://drive.google.com/open?id=1UEYXr47N9yD_9JxY457Vul4BwRizNVfv" rel="opener noreferrer" target="_blank">VIEW CV   <FaArrowCircleDown/></a>
+class CarouselImages extends Component {
+    render(){
+        return (
+            <div className={classes.carousel_container} id="start">
+                    {/* <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}> */}
+                <Carousel className={classes.carousel} dynamicHeight infiniteLoop={true} interval={6000} useKeyboardArrows={true} transitionTime={1700} emulateTouch showArrows={false} autoPlay  showStatus={false} showThumbs={false} >
+                    <div  className={classes.image_container} >
+                        <img className={classes.image} src={Hello}  alt="myImage"/>
+                        <div  className={classes.h1}>
+                            <h1 >I'm Mike</h1>
+                            <a href="https://drive.google.com/open?id=1UEYXr47N9yD_9JxY457Vul4BwRizNVfv" rel="opener noreferrer" target="_blank">VIEW CV   <FaArrowCircleDown/></a>
+                        </div>
                     </div>
-                </div>
-                <div className={classes.image_container}>
-                    <img className={classes.image} src={Pencil} alt="myImage"/>
-                    <div  className={classes.h2}> 
-                    <h1> I love building</h1>
-                    <h1>THINGS!!</h1>
-                    <a href="https://github.com/mittalsam98" rel="opener noreferrer" target="_blank">VIEW PROJECTS   <GoProject/></a>
+                    <div className={classes.image_container}>
+                        <img className={classes.image} src={Pencil} alt="myImage"/>
+                        <div  className={classes.h2}> 
+                        <h1> I love creating</h1>
+                        <h1>THINGS!!</h1>
+                        <a href="https://github.com/mittalsam98" rel="opener noreferrer" target="_blank">VIEW PROJECTS   <GoProject/></a>
+                        </div>
                     </div>
-                </div>
-                {/* <div className={classes.image_container}>
-                    <img className={classes.image} src={laptop1} alt="myImage"/>
-                    <div  className={classes.h1}> 
-                    </div>
-                </div> */}
-            </Carousel>
-          {/* </ScrollAnimation> */}
-        </div>
-     )
+                    {/* <div className={classes.image_container}>
+                        <img className={classes.image} src={laptop1} alt="myImage"/>
+                        <div  className={classes.h1}> 
+                        </div>
+                    </div> */}
+                </Carousel>
+            {/* </ScrollAnimation> */}
+            </div>
+        )
     }
 }
 export default CarouselImages
