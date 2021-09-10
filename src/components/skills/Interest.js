fabe980171f5b8bbaeecd4b4368e293ab9d69caf
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-09 23:19:57 -0500

    fixing skills sode

--- src/components/skills/Interest.js
+++ src/components/skills/Interest.js
@@ -9,24 +9,24 @@ class Interest extends Component {
       <div className={classes.box} id="interest">
         <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible={true}>
           <span className={classes.head}>WHAT I DO?</span>
-          <h2 className={classes.heading}>HERE ARE SOME OF MY EXPERTISE</h2>
+          <h2 className={classes.heading}>HERE ARE SOME OF MY SKILLS</h2>
           <div className={classes.Interest}>
             <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible={true}>
               <div className={classes.web}>
                 <h3>APP Develpoment</h3>
-                <p>I also have knowledge of flutter development and have experience in building android and IOS applications. I also have live app published on Google Play Store. </p>
+                <p>I have some experience working with PWA, allowing for interaction with offline applications. </p>
               </div>
             </ScrollAnimation>
             <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible={true}>
               <div className={classes.app}>
                 <h3>Web Develpoment</h3>
-                <p>I am a full stack web developer(MERN) and build websites using HTML, CSS, Javascript, ReactJs.<br /> I have also good grasp on Node and NoSQL(MongoDb)</p>
+                <p>I am a full stack web developer(MERN) and build websites using HTML, CSS, Javascript, ReactJs.<br /> I also have a good understanding of Node and NoSQL(MongoDb)</p>
               </div>
             </ScrollAnimation>
             <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible={true}>
               <div className={classes.other}>
-                <h3>Other's Interest</h3>
-                <p>I also have decent hand in developing website with WordPress.<br /> Adobe Photoshop, Adobe Illustrator, MS Office, C++, Github are my other fields of interest. </p>
+                <h3>More About Me</h3>
+                <p> Outside of creating different applications I really enjoy being outside! I love being outdoors whether it be golfing, fishing, hunting, or just hanging around a fire! If you cant find me at my computer, check outside! </p>
               </div>
             </ScrollAnimation>
           </div>
