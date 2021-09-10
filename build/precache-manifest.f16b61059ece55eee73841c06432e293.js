82480f4c0a4da4ef49ec3d235243af95996b5fcb
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-09 23:42:25 -0500

    publishing with github pages

---
+++ build/precache-manifest.f16b61059ece55eee73841c06432e293.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3391d5b55395071460cacc1565bac2b7",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "964b419f473d3bfd386c",
+    "url": "/react-portfolio/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "ef882318f7acff0b8084",
+    "url": "/react-portfolio/static/css/main.5aa064a0.chunk.css"
+  },
+  {
+    "revision": "964b419f473d3bfd386c",
+    "url": "/react-portfolio/static/js/2.201afe78.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/react-portfolio/static/js/2.201afe78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef882318f7acff0b8084",
+    "url": "/react-portfolio/static/js/main.0bd033ea.chunk.js"
+  },
+  {
+    "revision": "941f72dd1294e29efdea",
+    "url": "/react-portfolio/static/js/runtime-main.fe190204.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "/react-portfolio/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "fae3cbdfa2710f60ae85ac43f81d2125",
+    "url": "/react-portfolio/static/media/My-logo4.fae3cbdf.ico"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/react-portfolio/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "87561258a0fb17472008a4b2ff66696f",
+    "url": "/react-portfolio/static/media/laptop1.87561258.jpg"
+  }
+]);
