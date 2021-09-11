55bd9599bed9900c312480bf839695e1f59f7f2c
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-10 22:44:41 -0500

    adding projects

--- src/components/skills/Interest.js
+++ src/components/skills/Interest.js
@@ -9,24 +9,25 @@ class Interest extends Component {
       <div className={classes.box} id="interest">
         <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible={true}>
           <span className={classes.head}>WHAT I DO?</span>
-          <h2 className={classes.heading}>HERE ARE SOME OF MY SKILLS</h2>
+          <h2 className={classes.heading}>HERE ARE SOME OF MY PROJECTS</h2>
           <div className={classes.Interest}>
             <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible={true}>
               <div className={classes.web}>
-                <h3>APP Develpoment</h3>
-                <p>I have some experience working with PWA, allowing for interaction with offline applications. </p>
+                <img></img>
+                <h3>Paws and Pals</h3>
+                <button href="https://sleepy-cove-71764.herokuapp.com/" target="blank"> View App! </button>
               </div>
             </ScrollAnimation>
             <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible={true}>
               <div className={classes.app}>
-                <h3>Web Develpoment</h3>
-                <p>I am a full stack web developer(MERN) and build websites using HTML, CSS, Javascript, ReactJs.<br /> I also have a good understanding of Node and NoSQL(MongoDb)</p>
+                <h3> Blitz and Chaos</h3>
+                <button href="https://mgsteinmetz.github.io/BlitzandChaos/" target="blank">View App!</button>
               </div>
             </ScrollAnimation>
             <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible={true}>
               <div className={classes.other}>
-                <h3>More About Me</h3>
-                <p> Outside of creating different applications I really enjoy being outside! I love being outdoors whether it be golfing, fishing, hunting, or just hanging around a fire! If you cant find me at my computer, check outside! </p>
+                <h3>Work Day Scheduler</h3>
+                <button href="https://mgsteinmetz.github.io/work-day-scheduler" target="blank">View App!</button>
               </div>
             </ScrollAnimation>
           </div>
