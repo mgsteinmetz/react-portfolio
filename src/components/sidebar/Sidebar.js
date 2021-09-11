d198bc6fbd5504d67b2d9e6fd9eb15d8fd84103f
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-10 22:13:42 -0500

    fixing sidebar // adding resume

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import './Sidebar.css';
 import { HashLink as Link } from 'react-router-hash-link';
 import {  FiExternalLink } from 'react-icons/fi';
-import logo from '../images/My-logo4.ico'
+// import logo from '../images'
 
 class Sidebar extends Component {
     render() {
        
         return (
             <div className="sidebar">
-                <img src={logo} />
+                {/* <img src={logo} /> */}
                 <h1><Link smooth to="/#start" className="h1_links">Mike Steinmetz</Link></h1>
                 <p style={{color:'black',fontWeight:'bold'}} className="gmail"> mikesteinmetz00@gmail.com </p>
                 <ul className="sidebar-nav">
                     <li className="sidebar-nav-items"><Link smooth to="/#about" className="links">About Me</Link></li>
-                    <li className="sidebar-nav-items"><Link smooth to="/#education" className="links" >Education</Link></li>
-                    <li className="sidebar-nav-items"><Link smooth to="/#interest" className="links">Interest</Link></li>
-                    {/* <li className="sidebar-nav-items"><a href="http://www.bloggingpeek.com" target="_blank" rel="opener noreferrer" className="links"> Blog<FiExternalLink/></a></li> */}
+                    <li className="sidebar-nav-items"><Link smooth to="/#education" className="links" >Portfolio</Link></li>
+                    <li className="sidebar-nav-items"><Link smooth to="/#interest" className="links">Interests</Link></li>
+                    <li className="sidebar-nav-items"><Link smooth to="/#resume" className="links">Resume</Link></li>
                 </ul>
     
                 <div className="flip-card-back">
