54ba661716840ef5916033aa6286fcb01c69caf5
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-12 18:55:19 -0500

    adding resume, fixing sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -12,15 +12,18 @@ class Sidebar extends Component {
                 {/* <img src={logo} /> */}
                 <h1><Link smooth to="/#start" className="h1_links">Mike Steinmetz</Link></h1>
                 <p style={{color:'black',fontWeight:'bold'}} className="gmail"> mikesteinmetz00@gmail.com </p>
+
                 <ul className="sidebar-nav">
                     <li className="sidebar-nav-items"><Link smooth to="/#about" className="links">About Me</Link></li>
                     <li className="sidebar-nav-items"><Link smooth to="/#education" className="links" >Portfolio</Link></li>
                     <li className="sidebar-nav-items"><Link smooth to="/#interest" className="links">Interests</Link></li>
-                    <li className="sidebar-nav-items"><Link smooth to="/#resume" className="links">Resume</Link></li>
                 </ul>
     
                 <div className="flip-card-back">
+                <p style={{color:'black',fontWeight:'bold'}} className="contacts"> Contact Me Below!</p>
+
                     <ul className="sidebar-nav">
+                    <li className="sidebar-nav-icons"> <a href="./src/images/MIKE STEINMETZ RESUME.pdf" rel="opener noreferrer" target="_blank" class="fa fas fa-file fa-lg"></a></li>
                         <li className="sidebar-nav-icons"> <a href="https://www.linkedin.com/in/mike-steinmetz-9a989320a/" rel="opener noreferrer" target="_blank" class="fa fas fa-linkedin fa-lg"></a></li>
                         <li className="sidebar-nav-icons"> <a href="https://github.com/mgsteinmetz" rel="opener noreferrer" target="_blank" class="fa fas fa-github fa-lg"></a></li>
                         <li className="sidebar-nav-icons"> <a href="https://twitter.com/mikesteinmetz20" rel="opener noreferrer" target="_blank" className="fa fas fa-twitter fa-lg"></a></li>
