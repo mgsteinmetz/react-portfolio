d37f430da139f9d4cb7c084dbe72d2385307a749
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-12 19:45:50 -0500

    changing images

--- src/components/carousel/CarouselImages.js.js
+++ src/components/carousel/CarouselImages.js.js
@@ -4,9 +4,7 @@ import classes from './CarouselImages.module.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "animate.css/animate.min.css"; 
 import {GoProject} from 'react-icons/go'
-import {FaArrowCircleDown} from 'react-icons/fa'
 import Hello from '../images/Hello.jpg'
-import Pencil from '../images/Pencil.jpg'
 import laptop1 from '../images/laptop1.jpg'
 
 class CarouselImages extends Component {
@@ -18,16 +16,15 @@ class CarouselImages extends Component {
                     <div  className={classes.image_container} >
                         <img className={classes.image} src={Hello}  alt="myImage"/>
                         <div  className={classes.h1}>
-                            <h1 >I'm Mike</h1>
-                            <a href="https://drive.google.com/open?id=1UEYXr47N9yD_9JxY457Vul4BwRizNVfv" rel="opener noreferrer" target="_blank">VIEW CV   <FaArrowCircleDown/></a>
+                            <h1 >I'm Mike!</h1>
                         </div>
                     </div>
                     <div className={classes.image_container}>
-                        <img className={classes.image} src={Pencil} alt="myImage"/>
+                        <img className={classes.image} src={laptop1} alt="myImage"/>
                         <div  className={classes.h2}> 
-                        <h1> I love creating</h1>
-                        <h1>THINGS!!</h1>
-                        <a href="https://github.com/mgsteinmetz" rel="opener noreferrer" target="_blank">VIEW PROJECTS   <GoProject/></a>
+                        <h1>I love to code!</h1>
+                        <h1> ...And watch youtube</h1>
+                        <a href="https://github.com/mgsteinmetz" rel="opener noreferrer" target="blank">VIEW PROJECTS   <GoProject/></a>
                         </div>
                     </div>
                     {/* <div className={classes.image_container}>
