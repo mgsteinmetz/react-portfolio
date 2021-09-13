b31b01a6448243a39aa6919924855f2806ea5bcb
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-12 20:18:29 -0500

    adding cover image // adding projects

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './Sidebar.css';
 import { HashLink as Link } from 'react-router-hash-link';
 import {  FiExternalLink } from 'react-icons/fi';
-// import logo from '../images'
+import picture from '../images/mike 5.13.32 PM.jpg'
 
 class Sidebar extends Component {
     render() {
        
         return (
             <div className="sidebar">
-                {/* <img src={logo} /> */}
+                <img src={picture} />
                 <h1><Link smooth to="/#start" className="h1_links">Mike Steinmetz</Link></h1>
                 <p style={{color:'black',fontWeight:'bold'}} className="gmail"> mikesteinmetz00@gmail.com </p>
 
@@ -23,13 +23,13 @@ class Sidebar extends Component {
                 <p style={{color:'black',fontWeight:'bold'}} className="contacts"> Contact Me Below!</p>
 
                     <ul className="sidebar-nav">
-                    <li className="sidebar-nav-icons"> <a href="./src/images/MIKE STEINMETZ RESUME.pdf" rel="opener noreferrer" target="_blank" class="fa fas fa-file fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"> <a href="https://www.linkedin.com/in/mike-steinmetz-9a989320a/" rel="opener noreferrer" target="_blank" class="fa fas fa-linkedin fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"> <a href="https://github.com/mgsteinmetz" rel="opener noreferrer" target="_blank" class="fa fas fa-github fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"> <a href="https://twitter.com/mikesteinmetz20" rel="opener noreferrer" target="_blank" className="fa fas fa-twitter fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"> <a href="https://www.instagram.com/_msteinz_/" rel="opener noreferrer" target="_blank" class="fa fas fa-instagram fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"> <a href="mailto:mikesteinmetz00@gmail.com" rel="opener noreferrer" target="_blank" class="fa fas fa-envelope fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"> <a href="https://www.facebook.com" rel="opener noreferrer" target="_blank" class="fa fas fa-facebook fa-lg"></a></li>
+                    <li className="sidebar-nav-icons"> <a href="./src/images/MIKE STEINMETZ RESUME.pdf" rel="opener noreferrer" target="blank" class="fa fas fa-file fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://www.linkedin.com/in/mike-steinmetz-9a989320a/" rel="opener noreferrer" target="blank" class="fa fas fa-linkedin fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://github.com/mgsteinmetz" rel="opener noreferrer" target="blank" class="fa fas fa-github fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://twitter.com/mikesteinmetz20" rel="opener noreferrer" target="blank" className="fa fas fa-twitter fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://www.instagram.com/_msteinz_/" rel="opener noreferrer" target="blank" class="fa fas fa-instagram fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="mailto:mikesteinmetz00@gmail.com" rel="opener noreferrer" target="blank" class="fa fas fa-envelope fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://www.facebook.com" rel="opener noreferrer" target="blank" class="fa fas fa-facebook fa-lg"></a></li>
                     </ul>
                 </div>
               
--- src/components/skills/Interest.js
+++ src/components/skills/Interest.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import classes from './Interest.module.css';
 import ScrollAnimation from 'react-animate-on-scroll';
 import "animate.css/animate.min.css";
+import {GoProject} from 'react-icons/go'
+
 
 class Interest extends Component {
   render() {
@@ -13,21 +15,38 @@ class Interest extends Component {
           <div className={classes.Interest}>
             <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible={true}>
               <div className={classes.web}>
-                <img></img>
+              {/* <img className={classes.image} src={}  alt="myImage"/> */}
                 <h3>Paws and Pals</h3>
-                <button href="https://sleepy-cove-71764.herokuapp.com/" target="blank"> View App! </button>
+                <ul style={{ textAlign:'match-parent'}}>A pet shelter application built using:
+                  <li>Node.js and Express.js to create a RESTful API.</li>
+                  <li>Handlebars.js as the template engine.</li>
+                  <li>MySQL and the Sequelize ORM for the database.</li>
+                  <li>bcrypt, dotenv, RESTful Routes, ESLint.</li>
+                  <li>Multer to upload images.</li>
+                </ul>
+                <a href="https://sleepy-cove-71764.herokuapp.com/" target="blank"> View App! <GoProject/></a>
               </div>
             </ScrollAnimation>
             <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible={true}>
               <div className={classes.app}>
                 <h3> Blitz and Chaos</h3>
-                <button href="https://mgsteinmetz.github.io/BlitzandChaos/" target="blank">View App!</button>
+                <ul style={{ textAlign:'match-parent'}}>An interactive story application built using:
+                  <li>Bulma for CSS styling.</li>
+                  <li>Freesound.org for background music.</li>
+                  <li>Pexels and Art Intstitute of Chicago for background images.</li>
+                </ul>
+                <a href="https://mgsteinmetz.github.io/BlitzandChaos/" target="blank">View App!<GoProject/></a>
               </div>
             </ScrollAnimation>
             <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible={true}>
               <div className={classes.other}>
                 <h3>Work Day Scheduler</h3>
-                <button href="https://mgsteinmetz.github.io/work-day-scheduler" target="blank">View App!</button>
+                <ul style={{ textAlign:'match-parent'}}>A planner application built using:
+                  <li>Bootstrap for CSS styling.</li>
+                  <li>Luxon for current date and time.</li>
+                  <li>Raw Javascript.</li>
+                </ul>
+                <a href="https://mgsteinmetz.github.io/work-day-scheduler" target="blank">View App!<GoProject/></a>
               </div>
             </ScrollAnimation>
           </div>
